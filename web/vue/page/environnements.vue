1c5e4ea83495321d424177ab4e85601edf2cfb9f
CHABOUD <ludovic.chaboud@sogeti.com>
2018-06-11 10:03:07 +0200

    Descriptions

--- web/vue/page/environnements.vue
+++ web/vue/page/environnements.vue
@@ -1,68 +1,89 @@
 <template>
-  <div class="container-page">
-    <h1><span class="fa fa-server"></span> Environnements</h1>
-    <div class="environnements-wrapper">
-      <div class="tabs">
-        <div class="tab-header">
-          <ul class="nav nav-tabs">
-            <li class="nav-item"
+  <div class="environnements-page container-page">
+    <div class="row">
+      <div class="col-xs-12">
+        <h1><span class="fa fa-server"></span> Environnements</h1>
+      </div>
+      <div class="col-xs-12">
+        <div class="description">
+          Applications et conteneurs Docker par environnement / network.
+        </div>
+      </div>
+      <div class="col-xs-12">  
+        <div class="environnements-wrapper">
+          <div class="tabs">
+            <div class="tab-header">
+              <ul class="nav nav-tabs">
+                <li class="nav-item"
+                  v-for="(environnement, index) in environnements"
+                  v-bind:key="environnement.name"
+                >
+                  <a class="nav-link"
+                    v-on:click="viewTab(index)"
+                    v-bind:class="{ 
+                      active: tab === index
+                    }"
+                  >
+                    {{environnement.name}}
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="tab-content"
               v-for="(environnement, index) in environnements"
               v-bind:key="environnement.name"
-            >
-              <a class="nav-link"
-                v-on:click="viewTab(index)"
-                v-bind:class="{ 
-                  active: tab === index
-                }"
-              >
-                {{environnement.name}}
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div class="tab-content"
-          v-for="(environnement, index) in environnements"
-          v-bind:key="environnement.name"
-          v-bind:class="{ 
-            displayed: tab === index
-          }">
-          <div class="col-xs-3"
-            v-for="application in environnement.applications"
-            v-bind:key="application.name">
-            <div class="flat-card">
-              <div class="flat-card-title">
-                <h5>
-                  <span v-if="application.name === 'undefined'">Common</span>
-                  <span v-if="application.name !== 'undefined'">
-                    <b>{{application.name}}</b>
-                  </span>
-                  <span v-if="application.version !== 'undefined'">
-                    <br/>{{application.version}}
-                  </span>
-                </h5>
-              </div>
-              <div class="flat-card-bottom"
-                v-for="container in application.containers"
-                v-bind:key="container.Id"
-                v-bind:class="{ 
-                  'running-container': container.State === 'running',
-                  'exited-container': container.State === 'exited'
-                }">
-                <span 
-                  v-if="container.State === 'running'"
-                  class="fa fa-check icon-font running-font">
-                </span>
-                <span 
-                  v-if="container.State === 'exited'"
-                  class="fa fa-times icon-font exited-font">
-                </span>
-                <span v-for="name in container.Names" v-bind:key="name">{{name}}</span>
+              v-bind:class="{ 
+                displayed: tab === index
+              }">
+              <div class="col-xs-3"
+                v-for="application in environnement.applications"
+                v-bind:key="application.name">
+                <div class="flat-card">
+                  <div class="flat-card-title">
+                    <h5>
+                      <span v-if="application.name === 'undefined'">Common</span>
+                      <span v-if="application.name !== 'undefined'">
+                        <b>{{application.name}}</b>
+                      </span>
+                      <span v-if="application.version !== 'undefined'">
+                        <br/>{{application.version}}
+                      </span>
+                    </h5>
+                  </div>
+                  <div class="flat-card-bottom"
+                    v-for="container in application.containers"
+                    v-bind:key="container.Id"
+                    v-bind:class="{ 
+                      'running-container': container.State === 'running',
+                      'exited-container': container.State === 'exited'
+                    }">
+                    <span 
+                      v-if="container.State === 'running'"
+                      class="fa fa-check icon-font running-font">
+                    </span>
+                    <span 
+                      v-if="container.State === 'exited'"
+                      class="fa fa-times icon-font exited-font">
+                    </span>
+                    <span v-for="name in container.Names" v-bind:key="name">{{name}}</span>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
+        </div>  
+      </div>
+      <div class="col-xs-12">
+        <h2><span class="fa fa-light-bulb"></span> Indications</h2>
+        <div class="description">
+          Les conteneurs Docker sont liés à une application via les labels suivants:
+          <ul>
+            <li><code>application.name</code>: Nom de l'application</li>
+            <li><code>application.version</code>: Version de l'application</li>
+          </ul>
         </div>
       </div>
-    </div>  
+    </div>
   </div>
 </template>
 
@@ -86,7 +107,7 @@ module.exports = {
 
 <style>
 
-h1 {
+.environnements-page h1 {
   padding: 10px;
   margin-left: 10px;
   background-size: 50px;
@@ -99,7 +120,26 @@ h1 {
   border-bottom: 1px solid lightgray;
   padding-bottom: 0;
 }
-h1 .fa {
+.environnements-page h1 .fa {
+  font-size: 30px;
+}
+
+.environnements-page h2 {
+  margin-top: 30px;
+  padding: 30px 10px;
+  margin-left: 10px;
+  background-size: 50px;
+  height: 50px;
+  line-height: 50px;
+  font-size: 18px;
+  padding-top: 20px;
+  letter-spacing: 1px;
+  font-weight: 700;
+  border-bottom: 1px solid lightgray;
+  padding-bottom: 0;
+  color: gray;
+}
+.environnements-page h2 .fa {
   font-size: 30px;
 }
 
@@ -159,10 +199,6 @@ h1 .fa {
 }
 
 .tab-content {
-  background-color: white;
-}
-
-.tab-content {
   display: none;
   padding: 12px 0;
 }
@@ -207,6 +243,26 @@ h1 .fa {
 }
 .exited-font {
   color: #d01626;
+}
+
+.description {
+  display: block;
+  color: gray;
+  padding: 20px 20px 10px 20px;
+  font-size: 16px;
+}
+.description ul {
+  padding: 10px 20px 10px 20px;
+}
+.description li {
+  padding: 5px;
+  font-size: 16px;
+}
+.description code {
+  background-color: #efefef;
+  border-radius: 2px;
+  padding: 2px 8px;
+  font-size: 16px;
 }
 
 </style>
